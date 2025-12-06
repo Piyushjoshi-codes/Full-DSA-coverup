37936a715f811cdd7e5abb1e38d12ce868e0a5a8
Piyushjoshi-codes <piyushjoshi2150@gmail.com>
2025-12-06 09:32:09 +0530

    Added new changes to the code

--- Queues/Queue-using-STL.cpp
+++ Queues/Queue-using-STL.cpp
@@ -3,14 +3,25 @@
 using namespace std;
 int main() {
     queue<int> qq;
+
+    //Insertion on the rear end..
     qq.push(10);
     qq.push(20);
     qq.push(30);
+    cout << "Original size of the queue : ";
+    cout << qq.size();
     cout << "Queue" << endl;
     while (!qq.empty()) {
         cout << qq.front() << " ";
+
+        //Deletion at the front end..
         qq.pop();
     }
     cout << "\n";
+    cout << "Final size of the queue : ";
+    cout << qq.size();
     return 0;
 }
+
+//Insertion and deletion takes place on the rear and front end respectively...
+//Thus A queue is a linear data structure with such properties... 
