8e96eecda627d597346d46aef242ec1ca1109817
Piyushjoshi-codes <piyushjoshi2150@gmail.com>
2025-12-26 17:46:48 +0530

    Added code for binary search in cpp

---
+++ Searching Algorithm/Binary-Search.cpp
@@ -0,0 +1,32 @@
+//binary search in C++
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0;
+    int high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            return mid; // Return the index if the key is found
+        } else if (arr[mid] < key) {
+            low = mid + 1; // Search in the right half
+        } else {
+            high = mid - 1; // Search in the left half
+        }
+    }
+    return -1; // Return -1 if the key is not found
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 30;
+    int result = binarySearch(arr, n, key);
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found in the array." << endl;
+    }
+    return 0;
+}
